50593089b10887a2e4d99791872d89f9ed0fe8b6
Alex Chien <alex.chien@koocaa.com>
2011-07-11 16:15:59 +0800

    language switch js

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,13 +1,24 @@
 //menu
 $(document).ready(function(){
-  
-  $('li.mainlevel').mousemove(function(){
-  $(this).find('ul').slideDown();//you can give it a speed
-  });
-  $('li.mainlevel').mouseleave(function(){
-  $(this).find('ul').slideUp("fast");
-  });
-  
+
+	$('li.mainlevel').mousemove(function(){
+		$(this).find('ul').slideDown();//you can give it a speed
+	});
+	$('li.mainlevel').mouseleave(function(){
+		$(this).find('ul').slideUp("fast");
+	});
+
+	// language switcher
+	var curLang = document.location.href.split('html/')[1].split('/')[0];
+	var langs = ["cn","en"];
+	for(var i=0;i<langs.length;i++){
+		var theLang = langs[i];
+		if (theLang == curLang){
+			$('#lang_'+theLang).addClass('disabled').attr('href','#');
+		} else {
+			$('#lang_'+theLang).attr('href',document.location.href.replace('/'+curLang+'/','/'+theLang+'/'));
+		}
+	}
 });
 
 //ÀÁÈËÍ¼¿â www.lanrentuku.com
